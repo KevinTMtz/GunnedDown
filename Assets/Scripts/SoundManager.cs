7491424ceabc20c4de0e7d1e14462f841be9b6b8
Kevin Torres Martínez <44516784+KevinTMtz@users.noreply.github.com>
2020-03-04 20:03:25 -0600

    Created Few Folders

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip sound1;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound1 = Resources.Load<AudioClip>("Sounds/file_name");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound (string clip) {
+        switch (clip) {
+            case"asteroidEx":
+                audioSrc.PlayOneShot(sound1, 1f);
+                break;
+        }
+    }
+}
