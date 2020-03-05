a1313f0de2cc34f10693691c1d840bc72b393884
Kevin Torres Martínez <44516784+KevinTMtz@users.noreply.github.com>
2020-03-04 22:20:41 -0600

    Character Aim Point

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    public GameObject aimPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAimPoint();
+    }
+
+    // Move Aim Point
+    void MoveAimPoint() {
+        Vector3 aim = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        aim.Normalize();
+        aim *= -2f;
+        aimPoint.transform.localPosition = aim;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement attributes
+    Vector3 movementSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    // Player movement
+    void Movement() {
+        float movementFactor = 0.25f;
+        float speedX = Input.GetAxis("Horizontal") * movementFactor;
+        float speedY = Input.GetAxis("Vertical") * movementFactor;
+        movementSpeed = new Vector3(speedX, speedY, 0f);
+        transform.position = transform.position + movementSpeed;
+    }
+}
