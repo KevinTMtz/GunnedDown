7c77e2d733aae3e62e4f5a6cbefd7729af8e5300
Lars17S <efren.garibaldi@gmail.com>
2020-03-09 08:27:29 -0600

    BulletController added

---
+++ Assets/Resources/Scripts/Guns/Bullet/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Invoke("DestroyBullet", 1.5f);
+    }
+
+    // Destroy object after 1.5 seconds
+    void DestroyBullet() {
+        Destroy(gameObject);
+    }
+    
+    // Detects if the collision is an object Foreground
+    void OnCollisionEnter2D(Collision2D collision) {
+       if (collision.gameObject.CompareTag("Foreground"))
+            Destroy(gameObject);
+    }
+}
