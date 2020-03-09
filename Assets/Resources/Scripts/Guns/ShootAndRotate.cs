9ea9bdf221605ad295154009ab2a1cdf62ed1300
Kevin Torres Martínez <44516784+KevinTMtz@users.noreply.github.com>
2020-03-08 20:15:34 -0600

    Update ShootAndRotate.cs

--- Assets/Resources/Scripts/Guns/ShootAndRotate.cs
+++ Assets/Resources/Scripts/Guns/ShootAndRotate.cs
@@ -4,33 +4,28 @@ using UnityEngine;
 
 public class ShootAndRotate : MonoBehaviour
 {
+    // Gun
     private GameObject gun;
-    private float gunRotation;
     private Rigidbody2D rb;
-    private SpriteRenderer sr;
 
+    // Bullet
     private Transform shootPoint;
     private GameObject bullet;
     private float bulletForce = 50f;
     
+    // Player hand
     private GameObject hand;
     private Vector2 handPosition;
     
-    private PlayerAim playerAim;
+    // Gun Rotation
     private float aimAngle;
     private bool isFlipped = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        // To get aimAngle of PlayerAim
-        playerAim = GameObject.Find("Player").GetComponent("PlayerAim") as PlayerAim;
-
         // Get RigidBody of the gun
         rb = gameObject.GetComponent<Rigidbody2D>();
-
-        // Get Spriterenderer of the gun
-        sr = gameObject.GetComponent<SpriteRenderer>();
 
         // Get hand of player
         hand = GameObject.Find("Hand");
@@ -38,6 +33,8 @@ public class ShootAndRotate : MonoBehaviour
         // Get gun
         gun = GameObject.Find("Gun");
 
+        // TODO: If statement for that changes bullet depending on gun
+        // Get shootpoint transform and load bullet prefab
         shootPoint = GameObject.Find("ShootPoint").transform;
         bullet = (GameObject) Resources.Load("Prefabs/Bullets/Bullet", typeof(GameObject));
     }
@@ -45,8 +42,7 @@ public class ShootAndRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveAimPoint();
-
+        followAimPoint();
         moveWithHand();
         rotateGun();
 
@@ -55,12 +51,14 @@ public class ShootAndRotate : MonoBehaviour
         }
     }
 
+    // Instantiate bullet
     void Shoot() {
         GameObject bulletInstantiated = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
         Rigidbody2D bulletRB = bulletInstantiated.GetComponent<Rigidbody2D>();
         bulletRB.AddForce(shootPoint.right * bulletForce, ForceMode2D.Impulse);
     }
 
+    // Place gun in player hand
     void moveWithHand() {
         handPosition = hand.transform.position;
         transform.position = handPosition;
@@ -69,6 +67,7 @@ public class ShootAndRotate : MonoBehaviour
     void rotateGun() {
         rb.rotation = aimAngle;
 
+        // Rotate gun relative to aimpoint
         if (Mathf.Abs(aimAngle) > 90 && !isFlipped) {
             gameObject.transform.Rotate(180.0f, 0.0f, 0.0f, Space.Self);
             isFlipped = true;
@@ -77,6 +76,7 @@ public class ShootAndRotate : MonoBehaviour
             isFlipped = false;
         }
 
+        // Change the order in layer of gun and bullets
         if (aimAngle > 22.5 && aimAngle < 157.5) {
             gun.GetComponent<SpriteRenderer>().sortingOrder = 9;
             bullet.GetComponent<SpriteRenderer>().sortingOrder = 8;
@@ -87,7 +87,7 @@ public class ShootAndRotate : MonoBehaviour
     }
 
     // Move Aim Point
-    void MoveAimPoint() {
+    void followAimPoint() {
         // Get vector relative to gun and mouse position in camera
         Vector2 aim = GameObject.Find("ShootPoint").transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         aim *= -0.5f;
