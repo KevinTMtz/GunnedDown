098204696acc3bb3e1cc761efedfdfd049b65dd3
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-03-19 00:34:21 -0600

    Doors Implementation

---
+++ Assets/Resources/Scripts/Map/Door/DoorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public Animator animator;
+    //We should have an array of Objectives (for now they would be to defeat all enemies in a room)
+    private bool objectivesCompleted=true;
+    private void Open()
+    {
+        Debug.Log("Opening");
+        animator.SetBool("isOpen", true);
+    }
+    private void Close()
+    {
+        Debug.Log("Closing");
+        animator.SetBool("isOpen", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+        if(objectivesCompleted) Open();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (objectivesCompleted) Close();
+    }
+
+}
