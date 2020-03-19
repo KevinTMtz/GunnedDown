71d22bde5b53c50cdee5b41c123b9ddd7ab38474
Kevin Torres Martínez <44516784+KevinTMtz@users.noreply.github.com>
2020-03-19 13:41:16 -0600

    Pause Menu.

---
+++ Assets/Resources/Scripts/GUI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool pauseMenuActive;
+    public GameObject pauseMenuBackground;
+    public GameObject pauseMenu;
+
+    private Scene activeScene;
+    
+    void Start() {
+        Time.timeScale = 1;
+        
+        pauseMenuActive = false;
+        
+        activeScene = SceneManager.GetActiveScene();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
+    }
+
+    public void Pause() {
+        PlayMenuSound();
+        
+        if (!pauseMenuActive) {
+            pauseMenuActive = true;
+            pauseMenuBackground.SetActive(pauseMenuActive);
+            pauseMenu.SetActive(pauseMenuActive);
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        } else {
+            pauseMenuActive = false;
+            pauseMenuBackground.SetActive(pauseMenuActive);
+            pauseMenu.SetActive(pauseMenuActive);
+            Cursor.visible = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Restart() {
+        PlayMenuSound();
+        SceneManager.LoadScene(activeScene.name);
+    }
+
+    public void Continue() {
+        Pause();
+    }
+
+    public void ReturnToMenu() {
+        PlayMenuSound();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PlayMenuSound() {
+        SoundManager.PlaySound("MenuSound");
+    }
+}
--- Assets/Resources/Scripts/PlayerGuns/Shoot.cs
+++ Assets/Resources/Scripts/PlayerGuns/Shoot.cs
@@ -23,7 +23,7 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1") && Time.timeScale != 0) {
             ShootBullet();
         }
     }
